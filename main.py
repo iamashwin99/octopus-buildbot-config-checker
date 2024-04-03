0de5cc5aa85a15acabc065e52f5cd1c635eeadbd
iamashwin99 <ashwin-kumar.karnad@mpsd.mpg.de>
2024-04-03 08:57:12 +0200

    add an expander to reduce space

--- main.py
+++ main.py
@@ -9,25 +9,26 @@ st.set_page_config(page_title="Config Diff", page_icon=":smiley:", layout="wide"
 web_url="http://octopus-config-checker.streamlit.app/"
 # Display title and description
 st.title("Octopus Buildbot Config Checker")
-st.write(
-    "This is a simple app to compare two `config.h` files from Octopus buildbot runs."
-)
-st.write("- [Octopus Buildbot](https://www.octopus-code.org/buildbot)")
-st.write("- [Octopus-code](https://www.octopus-code.org)")
-st.write("Please enter the URLs of the two `config.h` files to compare.")
+with st.expander("Instructions"):
+    st.write(
+        "This is a simple app to compare two `config.h` files from Octopus buildbot runs."
+    )
+    st.write("- [Octopus Buildbot](https://www.octopus-code.org/buildbot)")
+    st.write("- [Octopus-code](https://www.octopus-code.org)")
+    st.write("Please enter the URLs of the two `config.h` files to compare.")
 
-# # Display example URLs
-# st.write("Example URLs:")
-# st.write("- [Builder 1](https://www.octopus-code.org/buildbot/#/builders/5/builds/611)")
-# st.write(
-#     "- [Builder 2](https://www.octopus-code.org/buildbot/#/builders/203/builds/13)"
-# )
-st.write("Click the 'Compare' button to compare the two files.")
-st.write(" You can also pass the URLs as query parameters in the URL. For example:")
-st.write(
-    "`http://octopus-config-checker.streamlit.app/?url1=https://www.octopus-code.org/buildbot/builders/5/builds/611&url2=https://www.octopus-code.org/buildbot/builders/203/builds/13`"
-)
-st.info("Note: The `#` directory in the URLs have been removed.")
+    # # Display example URLs
+    # st.write("Example URLs:")
+    # st.write("- [Builder 1](https://www.octopus-code.org/buildbot/#/builders/5/builds/611)")
+    # st.write(
+    #     "- [Builder 2](https://www.octopus-code.org/buildbot/#/builders/203/builds/13)"
+    # )
+    st.write("Click the 'Compare' button to compare the two files.")
+    st.write(" You can also pass the URLs as query parameters in the URL. For example:")
+    st.write(
+        "`http://octopus-config-checker.streamlit.app/?url1=https://www.octopus-code.org/buildbot/builders/5/builds/611&url2=https://www.octopus-code.org/buildbot/builders/203/builds/13`"
+    )
+    st.info("Note: The `#` directory in the URLs have been removed.")
 st.write("# Inputs")
 
 # Get user input for URLs and builder names
